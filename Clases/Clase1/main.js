930824b27432f3b2fa982de48668b3edf30ef1ff
MarceloFVillanueva <marcelo.villanueva@inceptia.ai>
2023-06-01 20:06:53 -0300

    Formas de definir variables

---
+++ Clases/Clase1/main.js
@@ -0,0 +1,22 @@
+// Comentario forma 1
+/* Comentario forma 2 */
+
+// Comentado rápido ctrl k c
+// Descomentado rápido ctrl k u
+
+// Tipos de Datos
+
+"Cadenas de caracteres"
+1
+true
+
+// Para definir variables, en ves de usar var (antes), usamos let (se guarda en memoria 19:54)
+
+let nombre;  // Definición de variable
+nombre = "Marcelo" // Asignación de variable
+
+let edad = "33" // Definición y asignación de variable
+
+// No se puede declarar una constante como 'const PI;' porque las constantes necesitan tener un valor inicializado, tampoco se le puede asignar otro valor luego
+
+const PI = 3.14;
