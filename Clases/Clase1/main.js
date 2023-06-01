877017cc199dd9f46bbaec1e122a5861f7e2f181
MarceloFVillanueva <marcelo.villanueva@inceptia.ai>
2023-06-01 20:18:59 -0300

    Formas de visualizar datos, el defer molestaba

--- Clases/Clase1/main.js
+++ Clases/Clase1/main.js
@@ -20,3 +20,32 @@ let edad = "33" // Definición y asignación de variable
 // No se puede declarar una constante como 'const PI;' porque las constantes necesitan tener un valor inicializado, tampoco se le puede asignar otro valor luego
 
 const PI = 3.14;
+
+// Visualización de datos e incorporación de datos
+
+/*
+    Formas:
+    console.log
+    alert()
+    prompt()
+    confirm()
+*/
+
+// console.log
+
+console.log(1)
+console.log("hola")
+console.log(1+1)
+
+// alert
+
+alert(1)
+alert("hola Mundo")
+alert(nombre)
+
+// Para que no molesten tanto los alert en la página se puede mostrar en el documento (20:16)
+
+document.write(1)
+document.write("hola Mundo")
+document.write(nombre)
+
