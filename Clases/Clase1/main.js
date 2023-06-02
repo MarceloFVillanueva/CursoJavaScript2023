293420e5a1d41ef88a444df3483b2bb2ae70010c
MarceloFVillanueva <marcelo.villanueva@inceptia.ai>
2023-06-01 21:21:07 -0300

    Ejercicio de práctica clase 1

---
+++ Clases/Clase1/main.js
@@ -0,0 +1,7 @@
+let nombre = prompt("Ingrese Nombre:")
+let apellido = prompt("Ingrese Apellido")
+
+let nombre_entero = nombre + " " + apellido
+alert("tu nombre es: " + nombre_entero)
+
+console.log("Nombre Completo: " + nombre_entero)
